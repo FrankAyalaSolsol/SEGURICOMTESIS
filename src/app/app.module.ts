102a85432decc20e3c41387d9142413c431cfe98
frank3000100@gmail.com <frank3000100@gmail.com>
2022-10-26 11:57:23 -0500

    Version Alfa V0.1 SEGURICOM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    InicioSesionComponent
   ],
   imports: [
     BrowserModule
