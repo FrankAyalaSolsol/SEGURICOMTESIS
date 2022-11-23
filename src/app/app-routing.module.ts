c1f67d9fbaba1b79648353172d37cb16d4c78516
Alvaro Fernandez <alvarofernandezmarcatoma@gmail.com>
2022-11-23 14:15:56 -0500

    Revert "Alpha 0.5"
    This reverts commit 7bfe77e6339c719ddf26ba1da16100b671c5f8fc.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { CrearCuentaComponent } from './components/crear-cuenta/crear-cuenta.component';
+import { PreEstudioComponent } from './components/pre-estudio/pre-estudio.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { PaginaInicioUsuarioComponent } from './pages/pagina-inicio-usuario/pagina-inicio-usuario.component';
+
+
+const routes: Routes = [
+  
+    { path: '', redirectTo:'/Inicio', pathMatch: 'full' },
+    {path: 'Inicio', component:InicioComponent},
+    {path: 'Inicio_Sesion', component:InicioSesionComponent},
+    {path: 'Acerca', component:AcercaComponent},
+    {path: 'Crear_Cuenta', component:CrearCuentaComponent},
+    {path: 'Pre_Estudio', component:PreEstudioComponent},
+    {path: 'PaginaInicioUsuario', component:PaginaInicioUsuarioComponent}
+  ];
+  
+  @NgModule({
+    imports: [
+      RouterModule.forRoot( routes ),
+
+    ],
+    exports: [ RouterModule ]
+  })
+  export class AppRoutingModule { }
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
-import { InicioComponent } from './components/inicio/inicio.component';
+
 import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
 import { AcercaComponent } from './components/acerca/acerca.component';
 import { CrearCuentaComponent } from './components/crear-cuenta/crear-cuenta.component';
 import { PreEstudioComponent } from './components/pre-estudio/pre-estudio.component';
-import { PaginaInicioUsuarioComponent } from './components/pagina-inicio-usuario/pagina-inicio-usuario.component';
-import { RouterModule, Routes } from '@angular/router';
 
-const appRoute: Routes = [
-  {path:'',redirectTo:'Inicio', pathMatch:'full'},
-  {path: '/Inicio', component:InicioComponent},
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { PaginaInicioUsuarioComponent } from './pages/pagina-inicio-usuario/pagina-inicio-usuario.component';
+
+/*const appRoute: Routes = [
+  {path:'',redirectTo:'/Inicio', pathMatch:'full'},
+  {path: '/components/Inicio', component:InicioComponent},
   {path: '/Inicio_Sesion', component:InicioSesionComponent},
   {path: '/Acerca', component:AcercaComponent},
   {path: '/Crear_Cuenta', component:CrearCuentaComponent},
   {path: '/Pre_Estudio', component:PreEstudioComponent},
   {path: '/PaginaInicioUsuario', component:PaginaInicioUsuarioComponent}
 ]
+
+*/
 
 @NgModule({
   declarations: [
@@ -31,10 +37,8 @@ const appRoute: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoute)
-    
+    AppRoutingModule,    
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
