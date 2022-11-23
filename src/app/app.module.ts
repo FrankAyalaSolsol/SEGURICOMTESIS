7bfe77e6339c719ddf26ba1da16100b671c5f8fc
frank3000100@gmail.com <frank3000100@gmail.com>
2022-11-23 12:36:07 -0500

    Alpha 0.4S

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,17 @@ import { AcercaComponent } from './components/acerca/acerca.component';
 import { CrearCuentaComponent } from './components/crear-cuenta/crear-cuenta.component';
 import { PreEstudioComponent } from './components/pre-estudio/pre-estudio.component';
 import { PaginaInicioUsuarioComponent } from './components/pagina-inicio-usuario/pagina-inicio-usuario.component';
-<<<<<<< HEAD
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoute: Routes = [
-  {path:'',redirectTo:'/Inicio', pathMatch:'full'},
-  {path: '/components/Inicio', component:InicioComponent},
+  {path:'',redirectTo:'Inicio', pathMatch:'full'},
+  {path: '/Inicio', component:InicioComponent},
   {path: '/Inicio_Sesion', component:InicioSesionComponent},
   {path: '/Acerca', component:AcercaComponent},
   {path: '/Crear_Cuenta', component:CrearCuentaComponent},
   {path: '/Pre_Estudio', component:PreEstudioComponent},
   {path: '/PaginaInicioUsuario', component:PaginaInicioUsuarioComponent}
 ]
-=======
-
->>>>>>> 839405baae41edfc0ffbdc118c8d5d6040b59c8e
-
 
 @NgModule({
   declarations: [
